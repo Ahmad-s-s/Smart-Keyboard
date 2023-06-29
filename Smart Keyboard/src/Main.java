2aa313cc85b17ec37e050c04a2a1d075106503b5
Ahmad-s-s <ahmadmshams2003@gmail.com>
2023-06-29 17:11:53 +0330

    first commit base of trie, spell checking and getting word from trie part 2.1, 2.2.2 and start of 2.2.1

---
+++ Smart Keyboard/src/Main.java
@@ -0,0 +1,8 @@
+import Methods.Functions;
+
+public class Main {
+    public static void main(String[] args) {
+        Structures.Trie mainTrie = Functions.read();
+//        Functions.AutoComplete(mainTrie, "a");
+    }
+}
