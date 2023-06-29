dca1599169a0d3745218ca4dab9b006e14a7aeb3
Ahmad-s-s <ahmadmshams2003@gmail.com>
2023-06-29 17:11:31 +0330

    first commit base of trie, spell checking and getting word from trie part 2.1, 2.2.2 and start of 2.2.1

---
+++ Smart Keyboard/src/Methods/Functions.java
@@ -0,0 +1,93 @@
+package Methods;
+
+import Structures.Trie;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Functions {
+    public static void addToTrie(Trie r, String word) {
+        for (int i = 0; i < word.length(); i++) {
+            int index = (int) word.charAt(i) - 'a';
+            if (r.children.get(index) == null) {
+                r.children.set(index, new Trie());
+            }
+            r.tChild += 1;
+            r.children.get(word.charAt(i) - 'a').val = word.charAt(i);
+            r.children.get(word.charAt(i) - 'a').parent = r;
+            r = r.children.get(word.charAt(i) - 'a');
+        }
+        r.isValid = true;
+    }
+
+    public static Trie read() {
+        ArrayList<String> words = new ArrayList<>();
+        Trie root = new Trie();
+
+        try {
+            File allWord = new File("D:\\Uni\\semester 4\\Data Structure\\Project\\DS_project\\src\\words.txt");
+            Scanner fScanner = new Scanner(allWord);
+            while (fScanner.hasNext()) {
+                words.add(fScanner.next().toLowerCase().replace("-", ""));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found !");
+            return null;
+        }
+        if (!words.isEmpty()) {
+            for (String string :
+                    words) {
+                addToTrie(root, string);
+            }
+        } else {
+            System.out.println("empty list");
+        }
+//        char[] s = new char[20];
+//        root.print(s, 0);
+        root.print();
+        return root;
+    }
+
+    public static ArrayList<String> AutoComplete(Trie mainTrie, String textFiled) {
+        Trie root = mainTrie;
+
+        for (int level = 0; level < textFiled.length(); level++) {
+
+            int index = textFiled.charAt(level) - 'a';
+            if (root.children.get(index) == null) {
+                break;
+            }
+            root = root.children.get(index);
+        }
+
+        ArrayList<String> textChildren = new ArrayList<>();
+
+        char[] s = new char[100];
+        textChildren = root.allChildren(s, 0);
+
+        System.out.println(textChildren.size());
+        for (String textChild : textChildren) {
+            System.out.println(textChild);
+        }
+        return textChildren;
+    }
+
+    public static boolean spellCheck(Trie mainTrie, String textFiled) {
+        Trie root = mainTrie;
+
+        for (int level = 0; level < textFiled.length(); level++) {
+
+            int index = textFiled.charAt(level) - 'a';
+
+            if (root.children.get(index) == null){
+                return false;
+            }
+
+            root = root.children.get(index);
+        }
+
+        return root.isValid;
+    }
+}
---
+++ Smart Keyboard/src/Structures/Trie.java
@@ -0,0 +1,92 @@
+package Structures;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Trie {
+
+    final static public int alphabet = 26;
+
+    public ArrayList<Trie> children;
+    public Trie parent;
+    public boolean isValid;
+    public int frequency;
+    public int tChild;
+
+    public char val;
+
+    public Trie () {
+        this.children = new ArrayList<>();
+        for (int i = 0; i < Trie.alphabet; i++) {
+            this.children.add(null);
+        }
+        this.isValid = false;
+        this.frequency = 0;
+        this.tChild = 0;
+        this.parent = null;
+    }
+
+    public void print(char[] s, int level) {
+        if (this.isValid) {
+            s[level] = '\0';
+            for (int i = 0; i < level; i++) {
+                System.out.print(s[i]);
+            }
+            System.out.println("   " + this.tChild);
+        }
+        for (int i = 0; i < Trie.alphabet; i++) {
+            if (this.children.get(i) != null) {
+                s[level] = (char) ((char) i + 'a');
+                this.children.get(i).print(s, level+1);
+            }
+        }
+    }
+
+    public void print() {
+        String s = this.getWord("", false);
+        if (s != "") {
+            System.out.println(s);
+        }
+        for (int i = 0; i < Trie.alphabet; i++) {
+            if (this.children.get(i) != null){
+                this.children.get(i).print();
+            }
+        }
+    }
+
+    public String getWord(String lastW, boolean between) {
+        if (this.parent == null) {
+            return lastW;
+        }else if (this.isValid || between){
+            return this.parent.getWord(this.val + lastW, true);
+        }
+        return "";
+    }
+
+    public ArrayList<String> allChildren (char[] s, int level) {
+
+        ArrayList<String> res = new ArrayList<>();
+
+        if (this.isValid){
+            String word = "";
+            for (int i = 0; i < level; i++) {
+                word.concat(String.valueOf(s[i]));
+            }
+            res.add(word);
+        }
+
+        ArrayList<String> childWord = new ArrayList<>();
+        for (int i = 0; i < Trie.alphabet; i++) {
+            if (this.children.get(i) != null) {
+                s[level] = (char) ((char) i + 'a');
+                childWord = this.children.get(i).allChildren(s, level+1);
+            }
+        }
+        for (String word :
+                childWord) {
+            res.add(word);
+        }
+
+        return res;
+    }
+}
